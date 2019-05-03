9ce909fa55e2a71443e1a909127db95419f4a307
Mwaniki <mr.mwaniki@gmail.com>
2019-05-03 17:51:56 +0300

    Review holder Quotes and test form

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,9 +9,9 @@ import { Quotes } from '../model/quotes';
 export class QuotesComponent implements OnInit {
   // public id:number, public submitter:string, public author:string, public quote: string, public postdate: Date, public upvote: number, public downvote: number)
   submitquotes = [  
+    new Quotes(1,"Daudi","Steve Jobs","When can only do Great Things when we do what we love!", new Date(1-5-2019), 124, 0),
     new Quotes(1,"Mwaniki","Chinua Achebe","When the Center is broken, Things fall apart", new Date(3-5-2019), 124, 0),
-    new Quotes(1,"Mwaniki","Chinua Achebe","When the Center is broken, Things fall apart", new Date(3-5-2019), 124, 0),
-    new Quotes(1,"Mwaniki","Chinua Achebe","When the Center is broken, Things fall apart", new Date(3-5-2019), 124, 0),
+    new Quotes(1,"Munene","Bill Gates","We are changing the Worls with Technology!", new Date(23-4-2019), 5657, 0),
 
   ]
 
